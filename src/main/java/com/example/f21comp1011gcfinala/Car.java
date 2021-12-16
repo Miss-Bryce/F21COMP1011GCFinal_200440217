a2d265dbbe552b1d6e810f49ccf2e112ac24c801
J. Bryce <53979775+Miss-Bryce@users.noreply.github.com>
2021-12-16 08:56:14 -0500

    Question 1 Complete?

--- src/main/java/com/example/f21comp1011gcfinala/Car.java
+++ src/main/java/com/example/f21comp1011gcfinala/Car.java
@@ -1,9 +1,69 @@
 package com.example.f21comp1011gcfinala;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
-public class Car {
+public class Car extends Dealership {
 
-    
+    private int id;
+    private int year;
+    private String make;
+    private double price;
+    private String model;
+    private ArrayList<String> type;
 
+
+    public int getId() {
+        return id;
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public ArrayList<String> getType() {
+        Collections.sort(type);
+        return type;
+    }
+
+    public void setType(ArrayList<String> type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + year + "]-[" + make + "] [" + model + "]- [$" + price + "]- Type(s): [" + type + "]";
+    }
+}
---
+++ src/main/java/com/example/f21comp1011gcfinala/Dealership.java
@@ -0,0 +1,15 @@
+package com.example.f21comp1011gcfinala;
+
+public class Dealership {
+    String dealership;
+
+    public void setDealership(String dealership) {
+        this.dealership = dealership;
+    }
+
+    public String getDealership() {
+        return dealership;
+    }
+
+
+}
