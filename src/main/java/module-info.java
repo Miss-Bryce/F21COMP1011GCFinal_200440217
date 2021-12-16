553f21ea3478a4c2e96e79d289310c83da79511d
J. Bryce <53979775+Miss-Bryce@users.noreply.github.com>
2021-12-16 09:11:00 -0500

    Question 2 Complete?

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.f21comp1011gcfinala {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires com.google.gson;
 
 
     opens com.example.f21comp1011gcfinala to javafx.fxml;
